41c82bf77c94bc9dd03f0e1e75fb1e7bda8f1a87
EmreAda <73690406+EmreAda@users.noreply.github.com>
2021-01-09 10:13:36 +0300

    Deleted unnecessary lines and added comments.

--- src/main/java/dev/azmeal/IOHandler/JsonHandler.java
+++ src/main/java/dev/azmeal/IOHandler/JsonHandler.java
@@ -9,40 +9,37 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class JsonHandler {
-    public enum Data {
+    public enum Data { //datas u gonna return. you can add more
         TOKEN, OWNERID, PREFIX
     }
 
     private final String PATH;
-    public JsonHandler(String path) {
+    public JsonHandler(String path) { //we need files path. right?
         PATH = path;
     }
 
 
 
     public String getData(Data data) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
+        JSONParser parser = new JSONParser(); //i think we gonna parse some shit
 
-        Reader reader = new FileReader(PATH);
-        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+        Reader reader = new FileReader(PATH); //i think we gonna parse this shit
+        JSONObject jsonObject = (JSONObject) parser.parse(reader); //omg! we actually did it
 
-        String dataToReturn;
+        String dataToReturn; //we gonna return this variable
         switch (data) {
             case TOKEN:
-                dataToReturn = (String) jsonObject.get("token");
-                //return (String) jsonObject.get("token");
+                dataToReturn = (String) jsonObject.get("token"); //i couldnt manage to return from just swtich it requires also from outside 
                 break;
             case OWNERID:
-                dataToReturn = (String) jsonObject.get("ownerid");
-                //return (String) jsonObject.get("ownerid");
+                dataToReturn = (String) jsonObject.get("ownerid"); //same
                 break;
             case PREFIX:
-                dataToReturn = (String) jsonObject.get("prefix");
-                //return (String) jsonObject.get("prefix");
+                dataToReturn = (String) jsonObject.get("prefix"); //same
                 break;
             default:
-                throw new IllegalStateException("Unexpected value: " + data);
+                throw new IllegalStateException("Unexpected value: " + data); //dunno what is this bitch. ide added but i think it is for unexpected params
         }
-        return dataToReturn;
+        return dataToReturn; //finally, some fuking good data
     }
 }
