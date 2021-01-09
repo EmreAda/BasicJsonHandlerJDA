38cad596a7cb187b525256a35878be8b09875241
EmreAda <73690406+EmreAda@users.noreply.github.com>
2021-01-09 10:32:07 +0300

    Some fkin good comment

--- src/main/java/dev/azmeal/Main.java
+++ src/main/java/dev/azmeal/Main.java
@@ -13,12 +13,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException, LoginException {
-        JsonHandler handler = new JsonHandler("/home/azmeal/IdeaProjects/ABotWithJsonHandler/src/main/java/dev/azmeal/config.json");
-        JDABuilder builder = JDABuilder.createDefault(handler.getData(JsonHandler.Data.TOKEN));
-        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
-        builder.setBulkDeleteSplittingEnabled(false);
-        builder.setCompression(Compression.NONE);
-        builder.setActivity(Activity.watching("Seni"));
-        builder.build();
+        JsonHandler handler = new JsonHandler("/home/azmeal/IdeaProjects/ABotWithJsonHandler/src/main/java/dev/azmeal/config.json"); //our json reader shit
+        JDABuilder builder = JDABuilder.createDefault(handler.getData(JsonHandler.Data.TOKEN)); //bot builder, i guess
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE); //same as down
+        builder.setBulkDeleteSplittingEnabled(false); //some code i copied from official docs
+        builder.setCompression(Compression.NONE); //same
+        builder.setActivity(Activity.watching("You!")); //status
+        builder.build(); //finally
     }
 }
